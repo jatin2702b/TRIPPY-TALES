1696fa1e358c2dc668015e5e8ed1d0f25d56a125
jatin2702b <jatin2702b@gmail.com>
2025-09-22 13:26:43 +0530

    added enquiry

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 // src/App.jsx
 import React from 'react';
 
-// Core sections
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Destinations from './components/Destinations';
 import Packages from './components/Packages';
-
-// New trekking slider section
 import Trekking from './components/Trekking';
-
 import ResortCatalog from './components/ResortCatalog';
 import Gallery from './components/Gallery';
 import About from './components/About';
 import Contact from './components/Contact';
 import FloatingActions from './components/FloatingActions';
 import Chatbot from './components/Chatbot';
+
+// New: end-of-page left sidebar
+import EnquirySidebarOnEnd from './components/EnquirySidebarOnEnd';
 
 export default function App() {
   return (
@@ -24,14 +23,22 @@ export default function App() {
       <Hero />
       <Destinations />
       <Packages />
-
-      {/* Trekking slider placed before ResortCatalog */}
       <Trekking />
-
       <ResortCatalog />
       <Gallery />
       <About />
       <Contact />
+
+      {/* Left-side static form, only when footer is in view */}
+      <EnquirySidebarOnEnd />
+
+      {/* Make sure your footer/root footer area has this id */}
+      <footer id="site-footer" className="border-t">
+        <div className="container mx-auto px-4 py-6 text-sm text-gray-600">
+          © {new Date().getFullYear()} Trippy Tales. All rights reserved.
+        </div>
+      </footer>
+
       <FloatingActions />
       <Chatbot />
     </div>
